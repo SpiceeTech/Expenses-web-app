1d2d801c2ce6dc49b1a98b72bb3998d2a5af921a
SpiceeTech <mongezinzuzo45@gmail.com>
2024-10-25 13:45:00 +0200

    Expenses: Full project to track expenses
    This commit completes the first full version of a web-based application
    that allows users to add, edit and delete their expenses. It also
    displays the total amount of all the expenses.

---
+++ Expenses/Models/Expense.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Expenses.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public decimal Value { get; set; }
+
+        [Required] public string? Description { get; set; }
+    }
+}
